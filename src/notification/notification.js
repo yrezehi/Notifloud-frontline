2e87649b4dd3214ddd350a537c327013e9fba243
unknown <yrezehi@gmail.com>
2023-10-05 04:36:46 -0700

    add: createNotificationElement

--- src/notification/notification.js
+++ src/notification/notification.js
@@ -8,24 +8,44 @@ var NotifloudNotification = function () {
     var notificationContainerElement;
     var notificationBellElement;
 
-    function toggle(){
-        if(notificationContainerElement.classList.contains(ACTIVE_CLASS)){
-            notificationContainerElement.classList.remove(ACTIVE_CLASS);
-        } else {
-            notificationContainerElement.classList.add(ACTIVE_CLASS);
-        }
-    }
-
     function off(){
         notificationContainerElement.style.right = `-${notificationContainerElement.parentNode.offsetWidth - notificationContainerElement.offsetWidth}px`;
-
         notificationBellElement.style.right = "0";
     }
 
     function on(){
         notificationContainerElement.style.right = "0";
-
         notificationBellElement.style.right = `-${notificationBellElement.parentNode.offsetWidth - notificationBellElement.offsetWidth}px`;
+    }
+
+    function set(notifications){
+        for(var notification of notifications){
+            createNotificationElement(notification["title"], notification["description"]);
+        }
+    }
+
+    function createNotificationElement(title, description){
+        return createElement(`
+            <div class="notification">
+                <div class="notification-title-container">
+                    <span class="notification-title">
+                        ${title}
+                    </span>
+                    <span class="notification-date">
+                        3 Hours ago
+                    </span>
+                </div>
+                <span class="notification-content">
+                    ${description}
+                </span>
+            </div>
+        `);
+    }
+
+    function createElement(serialized) {
+        var temporaryElement = document.createElement("div");
+        temporaryElement.innerHTML += serialized;
+        return temporaryElement.firstElementChild;
     }
 
     (function setup(){
@@ -39,8 +59,8 @@ var NotifloudNotification = function () {
     })();
 
     return function () {
-        return Object.freeze({ 
-            toggle: toggle
+        return Object.freeze({
+            set: set  
         });
     }();
 }();
